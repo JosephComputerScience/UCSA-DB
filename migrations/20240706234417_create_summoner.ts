ffbed04ba2328b49b83b4146b51e3d0aab9727c4
Joseph <jonguyen741@gmail.com>
2024-08-08 00:23:24 -0500

    add columns to summoner table

--- migrations/20240706234417_create_summoner.ts
+++ migrations/20240706234417_create_summoner.ts
@@ -4,6 +4,12 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('summoner', (table) => {
     table.string('summoner').notNullable();
     table.string('tag').notNullable();
+    table.string('summonerName').notNullable();
+    table.string('accountId').notNullable();
+    table.string('summonerId').notNullable();
+    table.integer('summonerLevel').notNullable();
+    table.integer('profileIconId').notNullable();
+    table.dateTime('updatedAt').notNullable();
     table.primary(['summoner', 'tag']);
   });
 }
