6f7adc79934033c8f107559742708d74055693e1
Joseph <jonguyen741@gmail.com>
2025-02-08 01:18:58 -0600

    add additional col

--- migrations/20240706234417_create_user.ts
+++ migrations/20240706234417_create_user.ts
@@ -2,11 +2,12 @@ import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('user', (table) => {
-    table.increments('id').primary(),
-    table.string('gameId').notNullable();
+    table.increments('id').primary();
+    table.string('userGameId').notNullable();
+    table.string('gameType').notNullable();
+    table.json('metadata');
     table.dateTime('updatedAt', { useTz: false }).notNullable();
     table.dateTime('lastManualUpdatedAt', { useTz: false }).notNullable();
-    table.json('metadata')
   });
 }
 
